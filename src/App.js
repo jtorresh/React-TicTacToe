1967a0ccafdb9741b1acec814a8ffa45b661ac85
JTorresHernandez <xeltronrenks@gmail.com>
2018-09-06 21:09:56 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class Square extends React.Component {
+
+  state = {
+      value: null,
+    };
+
   render() {
     return (
-    <div className="container">
-      <h1>Tic Tac Toe</h1>
-      <div className="board">
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-      </div>
-    </div>
+      <button className="square" onClick={()=> this.setState({value:'X'})}>
+      {this.state.value}
+      </button>
     );
   }
 }
 
-export default App;
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i}/>;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
